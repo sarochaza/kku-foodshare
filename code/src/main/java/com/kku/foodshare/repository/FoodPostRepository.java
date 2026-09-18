85af440b60a5010d07cf900b64b0f96732fbd087
sarochaza <sarocha.sao@kkumail.com>
2026-09-18 23:14:14 +0700

    feat: query active food posts

---
+++ code/src/main/java/com/kku/foodshare/repository/FoodPostRepository.java
@@ -0,0 +1,32 @@
+package com.kku.foodshare.repository;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.kku.foodshare.domain.entity.FoodPost;
+import com.kku.foodshare.domain.entity.FoodPostStatus;
+
+public interface FoodPostRepository
+        extends JpaRepository<FoodPost, Long> {
+
+    @Query("""
+            SELECT post
+            FROM FoodPost post
+            WHERE post.status IN :statuses
+              AND post.availableFrom <= :now
+              AND post.availableUntil > :now
+            ORDER BY post.availableUntil ASC
+            """)
+    List<FoodPost> findActiveMapPosts(
+            @Param("statuses")
+            Collection<FoodPostStatus> statuses,
+
+            @Param("now")
+            LocalDateTime now
+    );
+}
